60e59f659aa860cb16209b3099a1f41865bfbcb0
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-09-10 05:48:19 -0500

    remove db access from app struct

--- auth/internal/application/app.go
+++ auth/internal/application/app.go
@@ -9,7 +9,6 @@ import (
 )
 
 type App struct {
-	DataStore       *internal.DataStore
 	Confg           *config.Confg
 	UserRepository  repositories.UserRepositoryInterface
 	IdentityService services.IdentityServiceInterface
@@ -25,7 +24,6 @@ func BootstrapApp() (*App, error) {
 
 	logger.Info.Print("successfully connected to database")
 	return &App{
-		DataStore:       db, // TODO: take out -- db should only be accessed in repositories
 		Confg:           cfg,
 		UserRepository:  repositories.NewUserRepository(db.Client),
 		IdentityService: services.NewIdentityService(db.Client),
